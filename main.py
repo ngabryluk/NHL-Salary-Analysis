90dfacfd7e2fd322015b0ecbbcb0b7ecc9382db6
ngabryluk <ncgabryluk@gmail.com>
2024-10-03 15:13:17 -0400

    Data loaded in
    The data is now loaded into a pandas dataframe and I can now start to work with the data in Python.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import os
 
+def main():
 
+    filename = os.listdir(os.path.join(os.getcwd(), "data"))[0]
+    filepath = os.path.join(os.getcwd(), "data", filename)
+
+    df = pd.read_excel(filepath)
+
+    print(df)
+
+if __name__ == "__main__":
+    main()
